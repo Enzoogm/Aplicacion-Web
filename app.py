f7832e172f298d1550a9f2c1836169c4212d3b06
Enzo Materazzi <enzomtz008@gmail.com>
2025-03-26 12:11:10 -0300

    Agregar cuenta de 2 numeros ruta

--- app.py
+++ app.py
@@ -10,6 +10,20 @@ def home():
 def saludar():
     return '<h1>Aca no hay nada.</h1>'
 
+
+@app.route('//<int:n1></int:n2>')
+def suma (n1+n2)
+    suma= n1+n2
+    return f'<h1>{1} mas {2} da {suma}.</h1>'
+
+
+@app.route('/sumar/<int:n1></int:n2>')
+def suma (n1+n2)
+    suma= n1+n2
+    return f'<h1>{1} mas {2} da {suma}.</h1>'
+
+
 @app.route('/chau')
 def despedir():
     return '<h1>Aca menos.</h1>'
+    

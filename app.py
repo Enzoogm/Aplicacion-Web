1135d77993be680b2d2f16e81509cecd15c11a5f
Enzo Materazzi <enzomtz008@gmail.com>
2025-03-26 11:32:50 -0300

    Links y comentarios diferentes

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def home():
+    return '<a href="/hola">Hola</a>     <a href="/chau">Chau</a>'
+
+@app.route('/hola')
+def saludar():
+    return '<h1>Aca no hay nada.</h1>'
+
+@app.route('/chau')
+def despedir():
+    return '<h1>Aca menos.</h1>'
